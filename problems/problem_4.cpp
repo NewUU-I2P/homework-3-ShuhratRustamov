ad86e0cff8e74b39e31ddba019721579701a217e
Shuhrat Rustamov <144424418+ShuhratRustamov@users.noreply.github.com>
2023-11-14 19:39:29 +0500

    solved

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,14 +1,21 @@
 #include <string>
 #include <sstream>
 
-std::string problemSolution4(const std::string &macAddress) {
+
+std :: string problemSolution4(const std::string &macAddress) {
     // write your code here
     if (macAddress == "FF:FF:FF:FF:FF:FF"){
         return "Broadcast";
     }
-    else {
-
+    else if ((macAddress[1] >= 'A') % 2 != 0 && (macAddress[1] >= '0') % 2 == 0){
+        return "Multicast";
     }
+    else {
+        return "Unicast";
+    }
+
+
     // make use of control flow statements
     // return result;
 }
+
